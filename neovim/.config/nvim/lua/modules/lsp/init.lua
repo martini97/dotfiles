d17a7110bad4fe7774648582b496335efdfca953
Alessandro Martini <alessandrofmartini@gmail.com>
2021-03-28 15:35:29 -0300

    neovim: show virtualtext on diagnostic

--- neovim/.config/nvim/lua/modules/lsp/init.lua
+++ neovim/.config/nvim/lua/modules/lsp/init.lua
@@ -4,10 +4,6 @@ local completion = require("modules.lsp.completion")
 local servers = require("modules.lsp.servers")
 
 function M.setup()
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics,
-    {underline = true, virtual_text = false}
-  )
   completion.setup()
   servers.setup()
 end

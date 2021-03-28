bf90cd6a1b4f73135f5314f7c3984c60948188ea
Alessandro Martini <alessandrofmartini@gmail.com>
2021-03-28 20:26:28 -0300

    neovim: add formatting command with LSP

--- neovim/.config/nvim/lua/modules/lsp/init.lua
+++ neovim/.config/nvim/lua/modules/lsp/init.lua
@@ -6,6 +6,10 @@ local servers = require("modules.lsp.servers")
 function M.setup()
   completion.setup()
   servers.setup()
+
+  vim.cmd [[
+    command! -nargs=0 Format lua vim.lsp.buf.formatting()
+  ]]
 end
 
 return M

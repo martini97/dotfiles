90d48c5828ffd27d3559d590f895f500f8738122
Alessandro Martini <alessandrofmartini@gmail.com>
2021-04-02 20:26:03 -0300

    neovim: update configs

--- neovim/.config/nvim/lua/martini97/lsp/keymaps.lua
+++ neovim/.config/nvim/lua/martini97/lsp/keymaps.lua
@@ -5,77 +5,91 @@ local hover = require 'lspsaga.hover'
 local utils = require 'martini97.utils'
 
 local function has_lsp_capability(capability)
-  for _, client in ipairs(vim.lsp.get_active_clients()) do
-    if client.resolved_capabilities[capability] == true then
-      return true
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+        if client.resolved_capabilities[capability] == true then
+            return true
+        end
     end
-  end
-  return false
+    return false
 end
 
 local function code_action()
-  if has_lsp_capability('code_action') then
-    codeaction.code_action()
-  end
+    if has_lsp_capability('code_action') then codeaction.code_action() end
 end
 
 local function format()
-  if has_lsp_capability('document_formatting') then
-    vim.lsp.buf.formatting()
-  elseif has_lsp_capability('document_range_formatting') then
-    vim.lsp.buf.range_formatting()
-  else
-    utils.asyncformat()
-  end
+    if has_lsp_capability('document_formatting') then
+        vim.lsp.buf.formatting()
+    elseif has_lsp_capability('document_range_formatting') then
+        vim.lsp.buf.range_formatting()
+    else
+        utils.asyncformat()
+    end
 end
 
 local function go_to_definition()
-  if has_lsp_capability('goto_definition') then
-    vim.lsp.buf.definition()
-  end
+    if has_lsp_capability('goto_definition') then
+        vim.lsp.buf.definition()
+    else
+        utils.goto_tag()
+    end
 end
 
 local function show_docs()
-  if has_lsp_capability('hover') then
-    hover.render_hover_doc()
-  end
+    if has_lsp_capability('hover') then hover.render_hover_doc() end
 end
 
 local function find_references()
-  if has_lsp_capability('find_references') then
-    vim.lsp.buf.references()
-  else
-    utils.asyncgrep(nil, { full_word = true })
-  end
+    if has_lsp_capability('find_references') then
+        vim.lsp.buf.references()
+    else
+        utils.asyncgrep(nil, {full_word = true})
+    end
 end
 
-
 function M.set_keymaps(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  local opts = { noremap=true, silent=true }
+    local function buf_set_keymap(...)
+        vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    local function buf_set_option(...)
+        vim.api.nvim_buf_set_option(bufnr, ...)
+    end
+    local opts = {noremap = true, silent = true}
 
-  if client.resolved_capabilities.completion then
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-  end
+    if client.resolved_capabilities.completion then
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    end
 
-  vim.keymap.nnoremap {"<space>ca", code_action, silent = false, buffer = true}
-  vim.keymap.vnoremap {"<space>ca", code_action, silent = true, buffer = true}
-  vim.keymap.nnoremap {"<space>=", format, silent = true, buffer = true}
-  vim.keymap.vnoremap {"<space>=", format, silent = true, buffer = true}
-  vim.keymap.nnoremap {"gd", go_to_definition, silent = false, buffer = true}
-  vim.keymap.nnoremap {"K", show_docs, silent = true, buffer = true}
-  vim.keymap.nnoremap {"gh", find_references, silent = false, buffer = true}
+    vim.keymap.nnoremap {
+        "<space>ca",
+        code_action,
+        silent = false,
+        buffer = true
+    }
+    vim.keymap.vnoremap {"<space>ca", code_action, silent = true, buffer = true}
+    vim.keymap.nnoremap {"<space>=", format, silent = true, buffer = true}
+    vim.keymap.vnoremap {"<space>=", format, silent = true, buffer = true}
+    vim.keymap.nnoremap {"gd", go_to_definition, silent = false, buffer = true}
+    vim.keymap.nnoremap {"K", show_docs, silent = true, buffer = true}
+    vim.keymap.nnoremap {"gh", find_references, silent = false, buffer = true}
 
-  if client.resolved_capabilities.rename then
-    buf_set_keymap('n', 'gr', [[<cmd>lua require('lspsaga.rename').rename()<CR>]], opts)
-  end
+    if client.resolved_capabilities.rename then
+        buf_set_keymap('n', 'gr',
+                       [[<cmd>lua require('lspsaga.rename').rename()<CR>]], opts)
+    end
 
-  buf_set_keymap('n', '[d', [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]], opts)
-  buf_set_keymap('n', ']d', [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]], opts)
-  buf_set_keymap('n', '<C-k>', [[<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>]], opts)
-  buf_set_keymap('n', '<space>e', [[<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]], opts)
-  buf_set_keymap('n', '<space>q', [[<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>]], opts)
+    buf_set_keymap('n', '[d', [[<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>]],
+                   opts)
+    buf_set_keymap('n', ']d', [[<cmd>lua vim.lsp.diagnostic.goto_next()<CR>]],
+                   opts)
+    buf_set_keymap('n', '<C-k>',
+                   [[<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>]],
+                   opts)
+    buf_set_keymap('n', '<space>e',
+                   [[<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>]],
+                   opts)
+    buf_set_keymap('n', '<space>q',
+                   [[<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>]], opts)
 end
 
 return M
--- neovim/.config/nvim/lua/martini97/statusline.lua
+++ neovim/.config/nvim/lua/martini97/statusline.lua
@@ -1,7 +1,30 @@
 local M = {}
+local lualine = require 'lualine'
+local icons = require 'nvim-web-devicons'
 
 function M.setup()
-  require('lualine').setup{ options = { theme = 'dracula' } }
+    icons.setup()
+    lualine.setup {
+        options = {theme = 'dracula'},
+        sections = {
+            lualine_a = {{'mode', upper = true}},
+            lualine_b = {{'branch', icon = ''}},
+            lualine_c = {{'filename', file_status = true}},
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'}
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = {'filename'},
+            lualine_d = {},
+            lualine_x = {'location'},
+            lualine_y = {},
+            lualine_z = {}
+        },
+        extensions = {'fugitive'}
+    }
 end
 
 return M

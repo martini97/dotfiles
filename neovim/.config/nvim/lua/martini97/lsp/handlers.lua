8190ba1927fb16eb0bd32945b0bde4b24b1641f8
Alessandro Martini <alessandrofmartini@gmail.com>
2021-04-02 20:23:41 -0300

    neovim: format file

--- neovim/.config/nvim/lua/martini97/lsp/handlers.lua
+++ neovim/.config/nvim/lua/martini97/lsp/handlers.lua
@@ -2,42 +2,38 @@ local M = {}
 local handlers = {}
 local utils = require 'martini97.utils'
 
-handlers["textDocument/definition"] = function (default_handler)
-  return function (_, method, result)
-    if result ~= nil then
-      return default_handler(nil, method, result)
-    end
+handlers["textDocument/definition"] = function(default_handler)
+    return function(_, method, result)
+        if result ~= nil then return default_handler(nil, method, result) end
 
-    utils.goto_tag()
-  end
+        utils.goto_tag()
+    end
 end
 
-handlers["textDocument/references"] = function (default_handler)
-  return function (_, method, result)
-    -- if no results or only the current ocurrence was found.
-    if result ~= nil and #result > 1 then
-      return default_handler(nil, method, result)
-    end
+handlers["textDocument/references"] = function(default_handler)
+    return function(_, method, result)
+        -- if no results or only the current ocurrence was found.
+        if result ~= nil and #result > 1 then
+            return default_handler(nil, method, result)
+        end
 
-    utils.asyncgrep(nil, { full_word = true })
-  end
+        utils.asyncgrep(nil, {full_word = true})
+    end
 end
 
-handlers["textDocument/formatting"] = function (default_handler)
-  return function (_, method, result)
-    if result ~= nil then
-      return default_handler(nil, method, result)
-    end
+handlers["textDocument/formatting"] = function(default_handler)
+    return function(_, method, result)
+        if result ~= nil then return default_handler(nil, method, result) end
 
-    utils.asyncformat()
-  end
+        utils.asyncformat()
+    end
 end
 
 function M.setup()
-  for key, value in pairs(handlers) do
-    local default_handler = vim.lsp.handlers[key]
-    vim.lsp.handlers[key] = value(default_handler)
-  end
+    for key, value in pairs(handlers) do
+        local default_handler = vim.lsp.handlers[key]
+        vim.lsp.handlers[key] = value(default_handler)
+    end
 end
 
 return M

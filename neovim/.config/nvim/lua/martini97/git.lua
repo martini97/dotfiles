5e17a94b0fd281e1b0f68a7c79537453f839462c
Alessandro Martini <alessandrofmartini@gmail.com>
2021-04-02 20:22:31 -0300

    neovim: add git branches mapping

--- neovim/.config/nvim/lua/martini97/git.lua
+++ neovim/.config/nvim/lua/martini97/git.lua
@@ -1,9 +1,15 @@
 local M = {}
+local t_builtin = require 'telescope.builtin'
+
+local function git_branches()
+  t_builtin.git_branches()
+end
 
 function M.setup()
   vim.keymap.nnoremap {"<space>gs", ":Gstatus<cr>"}
   vim.keymap.nnoremap {"<space>gp", ":Gpull<cr>"}
   vim.keymap.nnoremap {"<space>gP", ":Gpush<cr>"}
+  vim.keymap.nnoremap {"<space>gb", git_branches}
 end
 
 return M
